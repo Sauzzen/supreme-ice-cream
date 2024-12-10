43d37e7753b2d9a4a37fc5d944a947d2c5d22aa4
Gian Carlo <nebulareclipse17@gmail.com>
2024-12-10 22:28:23 +0800

    Created LOG.md Modified app/schema.sql Modified app/templates/base.html

--- app/schema.sql
+++ app/schema.sql
@@ -4,7 +4,10 @@ CREATE TABLE
   user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    middle_name TEXT,
+    first_name TEXT NOT NULL
   );
 
 CREATE TABLE
@@ -12,5 +15,9 @@ CREATE TABLE
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER UNIQUE NOT NULL,
     balance INTEGER NOT NULL,
+    currency TEXT NOT NULL,
+    last_updated TEXT NOT NULL,
+    account_type TEXT CHECK (account_type IN "Savings", "Checking", "Credit") NOT NULL,
+    status TEXT CHECK (status IN "Active", "Inactive", "Suspended") NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
   );
